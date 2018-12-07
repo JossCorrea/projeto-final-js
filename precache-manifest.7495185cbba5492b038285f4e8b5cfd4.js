0e8af78872c14d7a54848c3680919f529d6050d1
JossCorrea <joseline.correa@gmail.com>
2018-12-07 11:07:17 -0200

    Updates

---
+++ precache-manifest.7495185cbba5492b038285f4e8b5cfd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "007916ec13ba8ec40a251abf610c473c",
+    "url": "/projeto-final-js/static/media/logo-reprograma.007916ec.png"
+  },
+  {
+    "revision": "f1a7bc71704fd778d913",
+    "url": "/projeto-final-js/static/js/runtime~main.f1a7bc71.js"
+  },
+  {
+    "revision": "eb807392970ff1342c8a",
+    "url": "/projeto-final-js/static/js/main.eb807392.chunk.js"
+  },
+  {
+    "revision": "e56082a3ddb16f475bc1",
+    "url": "/projeto-final-js/static/js/1.e56082a3.chunk.js"
+  },
+  {
+    "revision": "eb807392970ff1342c8a",
+    "url": "/projeto-final-js/static/css/main.1a6612b6.chunk.css"
+  },
+  {
+    "revision": "f1870e2afe59578bc8de2fbc81325f6c",
+    "url": "/projeto-final-js/index.html"
+  }
+];
